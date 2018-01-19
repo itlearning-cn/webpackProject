ef7c884e4c061a8dd8eec10d637c10b8310ed806
itlearning <stephen@uimake.cn>
2018-01-19 18:37:31 +0800

    initial

---
+++ README.md
@@ -0,0 +1,4 @@
+# webpackProject
+1. git clone https://github.com/itlearning-cn/webpackProject.git
+2. cd webpackProject
+3. npm install
---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        use: 'babel-loader',
+        test: /\.js$/
+      },
+      {
+        use: ['style-loader','css-loader'],
+        test:/\.css$/
+      }
+    ]
+  }
+};
